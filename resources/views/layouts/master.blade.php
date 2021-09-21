97358748a22689455cb0f52321d3524ec6825b55
emusisi4 <emusisi4@gmail.com>
2021-09-21 04:15:58 +0300

    Senna first commit

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -347,67 +347,47 @@ foreach ($allowedsubmenu as $rowallsub)
   
     <script src="/js/app.js"></script>
      <!-- Jquery Core Js -->
-    <script src="plugins/jquery/jquery.min.js"></script>
+    <script src="/plugins/jquery/jquery.min.js"></script>
 
     <!-- Bootstrap Core Js -->
-    <script src="plugins/bootstrap/js/bootstrap.js"></script>
+    <script src="/plugins/bootstrap/js/bootstrap.js"></script>
 
     <!-- Select Plugin Js -->
-    <script src="plugins/bootstrap-select/js/bootstrap-select.js"></script>
+    <script src="/plugins/bootstrap-select/js/bootstrap-select.js"></script>
 
     <!-- Slimscroll Plugin Js -->
-    <script src="plugins/jquery-slimscroll/jquery.slimscroll.js"></script>
+    <script src="/plugins/jquery-slimscroll/jquery.slimscroll.js"></script>
 
-    <!-- Bootstrap Colorpicker Js -->
-    <script src="plugins/bootstrap-colorpicker/js/bootstrap-colorpicker.js"></script>
-
-    <!-- Dropzone Plugin Js -->
-    <script src="plugins/dropzone/dropzone.js"></script>
-
-    <!-- Input Mask Plugin Js -->
-    <script src="plugins/jquery-inputmask/jquery.inputmask.bundle.js"></script>
 
     <!-- Multi Select Plugin Js -->
-    <script src="plugins/multi-select/js/jquery.multi-select.js"></script>
+    <script src="/plugins/multi-select/js/jquery.multi-select.js"></script>
 
     <!-- Jquery Spinner Plugin Js -->
-    <script src="plugins/jquery-spinner/js/jquery.spinner.js"></script>
+    <script src="/plugins/jquery-spinner/js/jquery.spinner.js"></script>
 
     <!-- Bootstrap Tags Input Plugin Js -->
-    <script src="plugins/bootstrap-tagsinput/bootstrap-tagsinput.js"></script>
+    <script src="/plugins/bootstrap-tagsinput/bootstrap-tagsinput.js"></script>
 
     <!-- noUISlider Plugin Js -->
-    <script src="plugins/nouislider/nouislider.js"></script>
+    <script src="/plugins/nouislider/nouislider.js"></script>
 
     <!-- Waves Effect Plugin Js -->
-    <script src="plugins/node-waves/waves.js"></script>
+    <script src="/plugins/node-waves/waves.js"></script>
 
     <!-- Custom Js -->
-    <script src="js/admin.js"></script>
-    <script src="js/pages/forms/advanced-form-elements.js"></script>
+    <script src="/js/admin.js"></script>
+    <script src="/js/pages/forms/advanced-form-elements.js"></script>
 
-    <script src="plugins/jquery-datatable/jquery.dataTables.js"></script>
-    <script src="plugins/jquery-datatable/skin/bootstrap/js/dataTables.bootstrap.js"></script>
-    <script src="plugins/jquery-datatable/extensions/export/dataTables.buttons.min.js"></script>
-    <script src="plugins/jquery-datatable/extensions/export/buttons.flash.min.js"></script>
-    <script src="plugins/jquery-datatable/extensions/export/jszip.min.js"></script>
-    <script src="plugins/jquery-datatable/extensions/export/pdfmake.min.js"></script>
-    <script src="plugins/jquery-datatable/extensions/export/vfs_fonts.js"></script>
-    <script src="plugins/jquery-datatable/extensions/export/buttons.html5.min.js"></script>
-    <script src="plugins/jquery-datatable/extensions/export/buttons.print.min.js"></script>
-<!-- Bootstrap DatePicker Css -->
-
-<script src="plugins/bootstrap-material-datetimepicker/js/bootstrap-material-datetimepicker.js"></script>
     <!-- Custom Js -->
-    <script src="js/admin.js"></script>
-    <script src="js/pages/tables/jquery-datatable.js"></script>
+    <script src="/js/admin.js"></script>
+ 
   
 <!--   
 <script>
 $.noConflict();
 // Code that uses other library's $ can follow here.
 </script> -->
-<!-- <script src="/js/app.js"></script> -->
+
 </body>
 
 </html>
